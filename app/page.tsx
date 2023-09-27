9ec246537a18a92a2fd009481d4964eba70731f3
Bishal Rana <rbishal50@gmail.com>
2023-09-27 09:57:27 +0545

    Add route groups and sample routes

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <section className="text-center">
-      <h1 className="h1-bold">The final Starter Base Project!</h1>
-      <h2 className="h2-bold">Stylings and linting setup!</h2>
-      <h3 className="h3-bold">Ready to start next js coding!</h3>
-    </section>
-  );
-}
