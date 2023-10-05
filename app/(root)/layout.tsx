99974e5e3dab99f1df8003cdfd9e07ea8a3edf03
Bishal Rana <rbishal50@gmail.com>
2023-10-05 09:24:56 +0545

    Add a shared navbar component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "@/components/shared/navbar/Navbar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <main className="background-light850_dark100 relative">
-      Navbar
+      <Navbar />
       <div className="flex">
         LefSidebar
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14">
