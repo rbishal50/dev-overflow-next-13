253a7fd89f0ed971cc68f18fa8e3dc3a57a271ff
Bishal Rana <rbishal50@gmail.com>
2023-10-08 10:01:06 +0545

    Map dummy questions and add a no data state and a component

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,8 +4,46 @@ import Filter from "@/components/shared/Filter";
 import LocalSearch from "@/components/shared/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
+import NoResult from "@/components/shared/NoResult";
 
 export default function Home() {
+  const questions = [
+    {
+      _id: "1",
+      title: "Cascading Deletes in SQLAlchemy?",
+      tags: [
+        { _id: "1", name: "python" },
+        { _id: "2", name: "sql" },
+      ],
+      author: {
+        _id: "1",
+        name: "John Doe",
+        picture: "john-doe.jpg",
+      },
+      upvotes: 1500000,
+      views: 500552,
+      answers: [],
+      createdAt: new Date("2023-09-01T12:00:00.000Z"),
+    },
+    {
+      _id: "2",
+      title: "How to center a div?",
+      tags: [
+        { _id: "3", name: "css" },
+        { _id: "4", name: "html" },
+      ],
+      author: {
+        _id: "2",
+        name: "Jane Smith",
+        picture: "jane-smith.jpg",
+      },
+      upvotes: 5,
+      views: 50,
+      answers: [],
+      createdAt: new Date("2021-09-02T10:30:00.000Z"),
+    },
+  ];
+
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -31,6 +69,18 @@ export default function Home() {
         />
       </div>
       <HomeFilters />
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {questions.length > 0 ? (
+          questions.map((question) => "QuestionCard")
+        ) : (
+          <NoResult
+            title="There’s no question to show"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the discussion. our query could be the next big thing others learn from. Get involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </>
   );
 }
