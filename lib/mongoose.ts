ce831459534bd9362ed4784fd1755c087cd66fb0
Bishal Rana <rbishal50@gmail.com>
2023-10-11 13:54:28 +0545

    Setup mongodb, server actions & connect to db

---
+++ lib/mongoose.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected: boolean = false;
+
+export const connectToDatabase = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL) {
+    return console.log("Missing mongo db url!!!");
+  }
+
+  if (isConnected) {
+    return console.log("Mongo db is already Connected!!!");
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "DevOverflow",
+    });
+    isConnected = true;
+    console.log("Mongo db is connected!!!");
+  } catch (error) {
+    console.log("Mongo db connection failure!!!", error);
+  }
+};
