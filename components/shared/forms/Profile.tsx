6d394db87d27f5acf8c21545d83831ae36956afe
Bishal Rana <rbishal50@gmail.com>
2023-11-22 14:57:32 +0545

    Add and populate the form in edit profile page

--- components/shared/forms/Profile.tsx
+++ components/shared/forms/Profile.tsx
@@ -1,4 +1,21 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { ProfileSchema } from "@/lib/validations";
 
 interface Props {
   clerkId: string;
@@ -6,7 +23,139 @@ interface Props {
 }
 
 const Profile = ({ clerkId, user }: Props) => {
-  return <div>Profile</div>;
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const parsedUser = JSON.parse(user);
+
+  const form = useForm<z.infer<typeof ProfileSchema>>({
+    resolver: zodResolver(ProfileSchema),
+    defaultValues: {
+      name: parsedUser.name ?? "",
+      username: parsedUser.username ?? "",
+      portfolioWebsite: parsedUser.portfolioWebsite ?? "",
+      location: parsedUser.location ?? "",
+      bio: parsedUser.bio ?? "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof ProfileSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="mt-9 flex w-full flex-col gap-9"
+      >
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem className="space-y-3.5">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Name <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Your name"
+                  className="no-focus paragraph-regular light-border-2 background-light700_dark300 text-dark300_light700 min-h-[56px]"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem className="space-y-3.5">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Username <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Your username"
+                  className="no-focus paragraph-regular light-border-2 background-light700_dark300 text-dark300_light700 min-h-[56px]"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="portfolioWebsite"
+          render={({ field }) => (
+            <FormItem className="space-y-3.5">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Portfolio Link
+              </FormLabel>
+              <FormControl>
+                <Input
+                  type="url"
+                  placeholder="Your portfolio website"
+                  className="no-focus paragraph-regular light-border-2 background-light700_dark300 text-dark300_light700 min-h-[56px]"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="location"
+          render={({ field }) => (
+            <FormItem className="space-y-3.5">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Location
+              </FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Where are you from?"
+                  className="no-focus paragraph-regular light-border-2 background-light700_dark300 text-dark300_light700 min-h-[56px]"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="bio"
+          render={({ field }) => (
+            <FormItem className="space-y-3.5">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Username <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="What's spectial about you?"
+                  className="no-focus paragraph-regular light-border-2 background-light700_dark300 text-dark300_light700 min-h-[56px]"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="mt-7 flex justify-end">
+          <Button
+            type="submit"
+            className="primary-gradient w-fit"
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "Saving" : "Submit"}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
 };
 
 export default Profile;
