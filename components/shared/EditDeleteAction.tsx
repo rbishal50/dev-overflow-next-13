1d32bbccfc0e555b6d3406a975509e9596d62efb
Bishal Rana <rbishal50@gmail.com>
2023-11-22 12:39:03 +0545

    Add a edit-delete action component

---
+++ components/shared/EditDeleteAction.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React from "react";
+import Image from "next/image";
+
+interface Props {
+  type: "Question" | "Answer";
+  itemId: string;
+}
+
+const EditDeleteAction = ({ type, itemId }: Props) => {
+  const handleEdit = () => {};
+  const handleDelete = () => {};
+
+  return (
+    <div className="flex items-center justify-end gap-3 max-sm:w-full">
+      {type === "Question" && (
+        <Image
+          src="/assets/icons/edit.svg"
+          alt="Edit"
+          width={14}
+          height={14}
+          className="cursor-pointer object-contain"
+          onClick={handleEdit}
+        />
+      )}
+      <Image
+        src="/assets/icons/trash.svg"
+        alt="Delete"
+        width={14}
+        height={14}
+        className="cursor-pointer object-contain"
+        onClick={handleDelete}
+      />
+    </div>
+  );
+};
+
+export default EditDeleteAction;
