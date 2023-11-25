3f79ccb473d1d6a1cea6003cb2a41c919f5e1a5b
Bishal Rana <rbishal50@gmail.com>
2023-11-25 12:17:59 +0545

    Implement local search in home page question cards

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -16,6 +16,7 @@ import User from "@/database/user.model";
 import { revalidatePath } from "next/cache";
 import Answer from "@/database/answer.model";
 import Interaction from "@/database/interaction.model";
+import { FilterQuery } from "mongoose";
 
 export async function createQuestion(params: CreateQuestionParams) {
   try {
@@ -82,10 +83,24 @@ export async function editQuestion(params: EditQuestionParams) {
   }
 }
 
-export async function getQuestions(_params: GetQuestionsParams) {
+export async function getQuestions(params: GetQuestionsParams) {
   try {
     connectToDatabase();
-    const questions = await Question.find({})
+    const { searchQuery } = params;
+    const query: FilterQuery<typeof Question> = {};
+
+    if (searchQuery) {
+      query.$or = [
+        {
+          title: { $regex: new RegExp(searchQuery, "i") },
+        },
+        {
+          content: { $regex: new RegExp(searchQuery, "i") },
+        },
+      ];
+    }
+
+    const questions = await Question.find(query)
       .populate({
         path: "tags",
         model: Tag,
