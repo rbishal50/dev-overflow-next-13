f4a6e14ad496a8c98940f6d99c7cf6634756672e
Bishal Rana <rbishal50@gmail.com>
2023-11-25 12:34:26 +0545

    Implement local search in tags page

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -34,10 +34,21 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   }
 }
 
-export async function getAllTags(_params: GetAllTagsParams) {
+export async function getAllTags(params: GetAllTagsParams) {
   try {
     connectToDatabase();
-    const tags = await Tag.find({});
+    const { searchQuery } = params;
+
+    const query: FilterQuery<typeof Tag> = {};
+
+    if (searchQuery) {
+      query.$or = [
+        { name: { $regex: new RegExp(searchQuery, "i") } },
+        { description: { $regex: new RegExp(searchQuery, "i") } },
+      ];
+    }
+
+    const tags = await Tag.find(query);
     return { tags };
   } catch (error) {
     console.log(error);
